9d90cb817e2fd4e3db4f7c449bb1959c62a3b357
Muppssy <muppssy@gmail.com>
2022-09-09 15:19:25 +0200

    Tuto Uploader PHP WIP

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div id="wrapper">
+    <label for="checkbox" class="btn">upload</label>
+    <input id="checkbox" type="checkbox" name="">
+    <div id="uploader">
+        <p> Glissez votre fichier ici ou cliquez ici</p>
+        <input id="file" type="file" name="file">
+        <label for="file" class="btn" for="file">Sélectionner un fichier</label>
+    </div>
+    <div id="gallery">
+        <div><img src="cadre-1.png" alt=""></div>
+        <div><img src="cadre-1.png" alt=""></div>
+    </div>
+    </div>
+</body>
+</html>
